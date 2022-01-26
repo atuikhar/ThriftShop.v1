bc484ab2206b3a93d88d702687ebe8d517edaa7b
atuikhar <atuikhar@gmail.com>
2022-01-26 12:43:59 +0530

    Database Image Changed

--- backend/data/products.js
+++ backend/data/products.js
@@ -3,7 +3,7 @@ const products = [
     name: 'Air Jordan 1 Retro High',
     image: '/images/AirJordan.jpg',
     description:
-      'Release Date February 23, 2018 Item Code 555088-610 Color GYM RED/BLACK/SUMMIT WHITE Size Report True to size: 87.9% Runs large: 6.6% Runs small: 5.5% No. of review: 1152 Made In China Style Trendy, Street Style Upper Material Synthetic Leather Functionality Slip-resistant Sole Material Rubber Sole Upper High Cut Toe Type Round Toe Heel type Flat heel Colorway Red/White/Black Firstly arrived in 1985, Air Jordan 1 has been around for over 3 decades. The Jordan line keeps revamping the OG silhouettes with fresh colours and premium materials, and has also collaborated with different artists, designers and retailers to capture the essence of the original through a modern lens.',
+      'Release Date February 23, 2018 Item Code 555088-610 Color GYM RED/BLACK/SUMMIT.',
     brand: 'Nike',
     category: 'Footwear',
     price: 89.99,
@@ -18,7 +18,7 @@ const products = [
 
     image: '/images/AdidasTshirt.jpg',
     description:
-      'One, two, three. Its that easy. 3-Stripes in contrast colours tell the world you are rolling with a community of rebels and rule-breakers. Show them off on this t-shirt and keep your adidas style authentic. Soft cotton feels cosy from the first wear to the millionth.',
+      'One, two, three. Its that easy. 3-Stripes in contr of rebels and rule-breakers.',
     brand: 'Adidas',
     category: 'Clothing',
     price: 599.99,
@@ -31,9 +31,9 @@ const products = [
   {
     name: 'Nike Sneakers',
 
-    image: '/images/Nike.jpg',
+    image: '/images/TaylorSmithNike.jpg',
     description:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     brand: 'Nike',
     category: 'Footwear',
     price: 929.99,
@@ -46,9 +46,9 @@ const products = [
   {
     name: 'Vans Of The Wall',
 
-    image: '/images/VansOfTheWall.jpg',
+    image: '/images/Vans.jpg',
     description:
-      'Vans Red T-Shirt Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      'Vans Red T-Shirt Lorem Ipsum is simply dummy text of the printing and typesetting industry. ',
     brand: 'Vans Of The Wall',
     category: 'Clothing',
     price: 399.99,
@@ -59,11 +59,11 @@ const products = [
     colorWay: ['Red', 'Green', 'Blue'],
   },
   {
-    name: 'Adidas ZX 500 Jordan',
+    name: 'Adidas',
 
-    image: '/images/AdidasZX500Jordan.jpg',
+    image: '/images/AdidasGrey.jpg',
     description:
-      'Adidas ZX 500 Jordan Closeup Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      'Adidas ZX 500 Jordan Closeup Lorem Ipsum is simply dummy text of the printing and typesetting ',
     brand: 'Adidas',
     category: 'Footwear',
     price: 49.99,
@@ -75,47 +75,15 @@ const products = [
     numReviews: 10,
   },
   {
-    name: 'SuperStar',
+    name: 'Nike SB Dunk',
 
-    image: '/images/SuperStar.jpg',
+    image: '/images/NikeDunk.jpg',
     description:
-      'Adidas SuperStar Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    brand: 'Adidas',
-    category: 'Footwear',
-    price: 29.99,
-    countInStock: 0,
-    sizes: [30, 35, 40],
-    colorWay: ['Red', 'Green', 'Blue'],
-
-    rating: 4,
-    numReviews: 12,
-  },
-  {
-    name: 'NikeAir',
-
-    image: '/images/Jordan.jpg',
-    description:
-      'NikeJordan Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+      'NikeJordan Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
     brand: 'Nike',
     category: 'Footwear',
     price: 29.99,
     countInStock: 3,
-    sizes: [30, 35, 40],
-    colorWay: ['Red', 'Green', 'Blue'],
-
-    rating: 4,
-    numReviews: 12,
-  },
-  {
-    name: 'Nike Collectors Edition',
-
-    image: '/images/NikeColl.jpg',
-    description:
-      'Collection Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    brand: 'Nike',
-    category: 'Footwear',
-    price: 29.99,
-    countInStock: 4,
     sizes: [30, 35, 40],
     colorWay: ['Red', 'Green', 'Blue'],
 
